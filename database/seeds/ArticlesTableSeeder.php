1cbe60efb93dec39f4c54f53b164a4957a0ff6ee
Nelson Gimón <nelson.gimon@gmail.com>
2021-06-21 04:23:31 -0500

    #5 Sedding para User

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Article;
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//        Article::truncate();
+//        $faker = \Faker\Factory::create();
+//
+//        for($i = 0;  $i > 30; $i++ ){
+//            Article::create([
+//               'title' => $faker->sentence,
+//               'body' => $faker->paragraph,
+//                'user_id' => rand(10, 20),
+//            ]);
+//        }
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        $faker = \Faker\Factory::create();
+
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
+
+        for($i = 0; $i > 30; $i++){
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->unique()->safeEmail,
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            ]);
+        }
+    }
+}
